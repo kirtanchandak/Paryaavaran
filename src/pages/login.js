855806244f40c4baaa79150963104d52809c3fa9
kirtanchandak <themarketingkirtan@gmail.com>
2023-03-06 20:52:16 +0530

    react-hook-form implemented

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,25 @@
 import Layout from "@/components/Layout";
 import React from "react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
 function LoginScreen() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
   return (
     <div>
       <Layout title="Login">
-        <form className="mx-auto max-w-screen-md">
+        <form
+          className="mx-auto max-w-screen-md"
+          onSubmit={handleSubmit(submitHandler)}
+        >
           <h1 className="font-bold text-xl mb-4">Login</h1>
           <div className="mb-4 flex flex-col">
             <label htmlFor="email" className="font-bold text-lg mb-1">
@@ -16,8 +29,12 @@ function LoginScreen() {
               type="email"
               id="email"
               autoFocus
-              className="border ring-green-200 focus:ring rounded "
+              className="border ring-green-200 focus:ring rounded"
+              {...register("email", { required: "Please Enter Email" })}
             />
+            {errors.email && (
+              <div className="text-red-500">{errors.email.message}</div>
+            )}
           </div>
           <div className="flex flex-col">
             <label htmlFor="password" className="text-lg mb-1 font-bold">
@@ -28,7 +45,17 @@ function LoginScreen() {
               id="password"
               autoFocus
               className="border mr-1 ring-green-200 focus:ring rounded"
+              {...register("password", {
+                required: "Please Enter Password",
+                minLength: {
+                  value: 6,
+                  message: "password should be more than 5 characters",
+                },
+              })}
             />
+            {errors.password && (
+              <div className="text-red-500">{errors.password.message}</div>
+            )}
           </div>
           <div className="mt-4">
             <button className="primary-btn">Login</button>
